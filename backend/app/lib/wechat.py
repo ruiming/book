93df4611e1dcac5f60c89eee70c31a0ddc45d079
KilerdChan <blove694@gmail.com>
2016-04-29 18:20:08 +0800

    using user_id

--- backend/app/lib/wechat.py
+++ backend/app/lib/wechat.py
@@ -9,9 +9,10 @@ def oauth(method):
     @functools.wraps(method)
     def warpper(*args, **kwargs):
         token = request.args.get('token', None)
+        user_id = request.args.get('user_id', None)
         url = request.url
         if token:
-            this_user = User.objects(wechat__access_token=token)
+            this_user = User.objects(wechat_openid=user_id, wechat__access_token=token)
             this_user = this_user.first() if this_user.count() == 1 else None
             if not this_user:
                 return redirect(wechat.authorize_url)

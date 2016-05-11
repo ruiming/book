b24be2896499ec4f16dac46181bd47d934f01964
Ruiming <20142100010@m.scnu.edu.cn>
2016-05-12 00:30:43 +0800

    use sessionStorage to speed up

--- app/module/me/me_ctrl.js
+++ app/module/me/me_ctrl.js
@@ -1,18 +1,32 @@
 routeApp.controller('MeCtrl',function($scope, $http) {
 
     // 返回用户信息，gravatar,name,cart,order{wait,received}
-    $http({
-        method: 'GET',
-        url: host + '/user_info'
-    }).success(function(response){
-        $scope.user = response;
-        $scope.user.cart = 5;
-        $scope.user.order = {
-            wait: 4,
-            received: 3
-        };
-        sessionStorage.name = $scope.user.username;
-        sessionStorage.avatar = $scope.user.avatar;
-    });
+    if(sessionStorage.user != undefined) {
+        $scope.user = JSON.parse(sessionStorage.user);
+        $http({
+            method: 'GET',
+            url: host + '/user_info'
+        }).success(function(response){
+            $scope.user.cart = response.cart_num;
+            $scope.user.order = {
+                wait: 0,
+                received: 0
+            };
+        });
+    }
+    else {
+        $http({
+            method: 'GET',
+            url: host + '/user_info'
+        }).success(function(response){
+            $scope.user = response;
+            $scope.user.order = {
+                wait: 0,
+                received: 0
+            };
+            sessionStorage.user = JSON.stringify(response);
+        });
+    }
+
     
 });

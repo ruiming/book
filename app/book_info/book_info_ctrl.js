55c0b0c0f22135fba5a44b1ad556ddabec91ef86
ruiming <ruiming.zhuang@gmail.com>
2016-07-23 16:53:59 +0800

    Update

--- app/book_info/book_info_ctrl.js
+++ app/book_info/book_info_ctrl.js
@@ -1,18 +1,15 @@
-angular
-    .module('index')
-    .controller('BookInfoCtrl', function($http, $scope, $stateParams){
-    $scope.busy = true;
-    
-    // 获取图书信息(包含评论和标签)
-    $http({
-        method: 'GET',
-        url: host + '/book',
-        params: {
-            isbn: $stateParams.isbn,
-            type: "detail"
-        }
-    }).success(function(response){
-        $scope.book = response;
-        $scope.busy = false;
-    });
-});
+(function(){
+    "use strict";
+
+    angular
+        .module('index')
+        .controller('BookInfoCtrl', function($http, $scope, bookservice, $stateParams) {
+            $scope.busy = true;
+
+            bookservice.getBookDetail($stateParams.isbn).then(response => {
+                $scope.book = response;
+                $scope.busy = false;
+            });
+        });
+
+})();

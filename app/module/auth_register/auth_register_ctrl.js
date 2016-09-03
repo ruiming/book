516a6e8492e5d86827653048944eecd230a670cb
Ruiming <ruiming.zhuang@gmail.com>
2016-09-03 14:05:33 +0800

    Add register form validation

--- app/module/auth_register/auth_register_ctrl.js
+++ app/module/auth_register/auth_register_ctrl.js
@@ -6,6 +6,16 @@
         .controller('AuthRegisterCtrl', AuthRegisterCtrl);
 
     function AuthRegisterCtrl() {
+        let vm = this;
+        vm.alerts = [];
+        vm.register = register;
 
+        function register(user) {
+            vm.alerts = [];
+            if(vm.form.name.$invalid) vm.alerts.push("用户名不能为空");
+            if(vm.form.password.$error.required) vm.alerts.push("密码不能为空");
+            else if(vm.form.password.$error.minlength) vm.alerts.push("密码须大于6位");
+            if(vm.form.password.$modelValue !== void 0 && vm.form.password.$modelValue !== vm.form.password_repeat.$modelValue) vm.alerts.push("两次密码不匹配");
+        }
     }
 })();
